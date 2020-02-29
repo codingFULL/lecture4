4528531b213c259b6d15e0aab3e12f1822cd2a92
Andres Camilo Jimenez Alvarez <jime112372@siigo.com>
2020-02-29 12:29:44 -0500

    Se agrega el js

---
+++ scripts/jsTest1.js
@@ -0,0 +1,26 @@
+function add() {
+  let val1 = document.getElementById('text1').value;
+  let val2 = document.getElementById('text2').value;
+  window.alert(Number(val1) + Number(val2));
+}
+
+function div() {
+  let val1 = document.getElementById('text1').value;
+  let val2 = document.getElementById('text2').value;
+  window.alert(Number(val1) / Number(val2));
+}
+
+function add1() {
+  let val1 = document.getElementById('text1');
+  val1.value = val1.value + '1';
+}
+
+function add2() {
+  let val1 = document.getElementById('text1');
+  val1.value = val1.value + '2';
+}
+
+function add3() {
+  let val1 = document.getElementById('text1');
+  val1.value = val1.value + '3';
+}
